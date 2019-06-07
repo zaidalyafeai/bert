2d652c039ddd702b1fd4fbcf7f2cc3b65e1879b1
Zaid Alyafeai <alyafey22@gmail.com>
2019-06-07 04:17:40 +0300

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -353,7 +353,7 @@ class ColaProcessor(DataProcessor):
 
   def get_labels(self):
     """See base class."""
-    return ["0", "1"]
+    return [str(x) for x in range(14)]
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
